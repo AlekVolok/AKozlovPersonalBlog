db35b5ea56f9dd89b80c4e489b16479bfec85385
AlekVolok <92682283+AlekVolok@users.noreply.github.com>
2023-10-15 23:31:14 +0200

    add hidden page

---
+++ js/learn_words_game.js
@@ -0,0 +1,72 @@
+let selectedLeft = null;
+let selectedRight = null;
+let pairs = {};
+
+function checkPair(leftWord, rightWord) {
+    if (pairs[leftWord] === rightWord) {
+        $('#leftWords').find(`[data-word="${leftWord}"]`).addClass('correct').off();
+        $('#rightWords').find(`[data-word="${rightWord}"]`).addClass('correct').off();
+
+        if ($('.correct').length === Object.keys(pairs).length * 2) {
+            alert("Congratulations! You matched all pairs!");
+        }
+    } else {
+        $('#leftWords').find(`[data-word="${leftWord}"]`).addClass('incorrect');
+        $('#rightWords').find(`[data-word="${rightWord}"]`).addClass('incorrect');
+        setTimeout(() => {
+            $('.incorrect').removeClass('incorrect');
+        }, 1000);
+    }
+    selectedLeft = null;
+    selectedRight = null;
+}
+
+function loadWords() {
+    const topic = $('#topicSelect').val();
+    $.getJSON(`words_database/topics/${topic}.json`, function(data) {
+        pairs = data;
+        $('#leftWords, #rightWords').empty();
+
+        // Get a random sample of up to 10 pairs
+        const samplePairs = Object.entries(pairs).sort(() => 0.5 - Math.random()).slice(0, 10);
+        const leftWords = samplePairs.map(pair => pair[0]);
+        const rightWords = samplePairs.map(pair => pair[1]).sort(() => Math.random() - 0.5); // Shuffle
+
+        leftWords.forEach(word => {
+            const btn = $(`<button data-word="${word}">${word}</button>`);
+            btn.click(function() {
+                if (selectedRight) {
+                    checkPair(word, selectedRight);
+                } else {
+                    selectedLeft = word;
+                }
+            });
+            $('#leftWords').append(btn);
+        });
+
+        rightWords.forEach(word => {
+            const btn = $(`<button data-word="${word}">${word}</button>`);
+            btn.click(function() {
+                if (selectedLeft) {
+                    checkPair(selectedLeft, word);
+                } else {
+                    selectedRight = word;
+                }
+            });
+            $('#rightWords').append(btn);
+        });
+    });
+}
+
+// Load topics and initialize game
+$(document).ready(function() {
+    $.getJSON("words_database/topics.json", function(data) {
+        data.forEach(topic => {
+            $('#topicSelect').append(`<option value="${topic}">${topic}</option>`);
+        });
+        loadWords();
+    });
+
+    // Bind the change event to the dropdown to load words when the topic changes
+    $('#topicSelect').change(loadWords);
+});
