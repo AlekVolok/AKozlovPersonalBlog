1565a13f024545ae86b909aec1bea073db3fd6a1
AlekVolok <92682283+AlekVolok@users.noreply.github.com>
2023-10-16 00:36:27 +0200

    added new unused words logics

--- js/learn_words_game.js
+++ js/learn_words_game.js
@@ -1,16 +1,46 @@
 let selectedLeft = null;
 let selectedRight = null;
 let pairs = {};
+let displayedPairs = {}; // Keep track of displayed pairs
+let correctlyGuessedPairs = 0; // Counter for correctly guessed pairs
+
+function loadNewSetOfPairs() {
+    correctlyGuessedPairs = 0;
+    $('#leftWords, #rightWords').empty();
+
+    const remainingPairsList = Object.entries(pairs).filter(pair => !displayedPairs[pair[0]]);
+    const samplePairs = remainingPairsList.sort(() => 0.5 - Math.random()).slice(0, 10);
+    const leftWords = samplePairs.map(pair => pair[0]);
+    const rightWords = samplePairs.map(pair => pair[1]).sort(() => Math.random() - 0.5); // Shuffle
+
+    leftWords.forEach(word => {
+        const btn = $(`<button data-word="${word}">${word}</button>`);
+        btn.click(function() {
+            onLeftClick(word);
+        });
+        $('#leftWords').append(btn);
+        displayedPairs[word] = true;
+    });
+
+    rightWords.forEach(word => {
+        const btn = $(`<button data-word="${word}">${word}</button>`);
+        btn.click(function() {
+            onRightClick(word);
+        });
+        $('#rightWords').append(btn);
+    });
+}
 
 function checkPair(leftWord, rightWord) {
     $('.selected').removeClass('selected'); // Remove the yellow highlight from any selected word
-    
+
     if (pairs[leftWord] === rightWord) {
+        correctlyGuessedPairs += 1;
         $('#leftWords').find(`[data-word="${leftWord}"]`).addClass('correct').off();
         $('#rightWords').find(`[data-word="${rightWord}"]`).addClass('correct').off();
 
-        if ($('.correct').length === Object.keys(pairs).length * 2) {
-            alert("Congratulations! You matched all pairs!");
+        if (correctlyGuessedPairs === 10) {
+            loadNewSetOfPairs();
         }
     } else {
         $('#leftWords').find(`[data-word="${leftWord}"]`).addClass('incorrect');
@@ -21,66 +51,37 @@ function checkPair(leftWord, rightWord) {
     }
     selectedLeft = null;
     selectedRight = null;
+}
+
+function onLeftClick(word) {
+    $('.selected').removeClass('selected'); // Ensure only one word is highlighted
+    if (selectedRight) {
+        checkPair(word, selectedRight);
+        selectedRight = null;
+    } else {
+        selectedLeft = word;
+        $(`#leftWords [data-word="${word}"]`).addClass('selected');
+    }
+}
+
+function onRightClick(word) {
+    $('.selected').removeClass('selected'); // Ensure only one word is highlighted
+    if (selectedLeft) {
+        checkPair(selectedLeft, word);
+        selectedLeft = null;
+    } else {
+        selectedRight = word;
+        $(`#rightWords [data-word="${word}"]`).addClass('selected');
+    }
 }
 
 function loadWords() {
     const topic = $('#topicSelect').val();
     $.getJSON(`words_database/topics/${topic}.json`, function(data) {
         pairs = data;
-        $('#leftWords, #rightWords').empty();
-
-        // Get a random sample of up to 10 pairs
-        const samplePairs = Object.entries(pairs).sort(() => 0.5 - Math.random()).slice(0, 10);
-        const leftWords = samplePairs.map(pair => pair[0]);
-        const rightWords = samplePairs.map(pair => pair[1]).sort(() => Math.random() - 0.5); // Shuffle
-
-        leftWords.forEach(word => {
-            const btn = $(`<button data-word="${word}">${word}</button>`);
-            btn.click(function() {
-                if (selectedRight) {
-                    checkPair(word, selectedRight);
-                } else {
-                    selectedLeft = word;
-                    $(this).addClass('selected'); // Highlight the selected word with yellow
-                }
-            });
-            $('#leftWords').append(btn);
-        });
-
-        rightWords.forEach(word => {
-            const btn = $(`<button data-word="${word}">${word}</button>`);
-            btn.click(function() {
-                if (selectedLeft) {
-                    checkPair(selectedLeft, word);
-                } else {
-                    selectedRight = word;
-                    $(this).addClass('selected'); // Highlight the selected word with yellow
-                }
-            });
-            $('#rightWords').append(btn);
-        });
+        displayedPairs = {};
+        loadNewSetOfPairs();
     });
-}
-
-function checkPair(leftWord, rightWord) {
-    $('.selected').removeClass('selected'); // Remove the yellow highlight from any selected word
-    
-    if (pairs[leftWord] === rightWord) {
-        $('#leftWords').find(`[data-word="${leftWord}"]`).addClass('correct').off();
-        $('#rightWords').find(`[data-word="${rightWord}"]`).addClass('correct').off();
-
-        if ($('.correct').length === Object.keys(pairs).length * 2) {
-            $('#congratulations').show();  // Display the congratulatory message
-        }
-    } else {
-        $('#leftWords').find(`[data-word="${leftWord}"]`).addClass('incorrect');
-        $('#rightWords').find(`[data-word="${rightWord}"]`).addClass('incorrect');
-        setTimeout(() => {
-            $('.incorrect').removeClass('incorrect');
-        }, 1000);
-    }
-    selectedLeft = null;
-    selectedRight = null;
 }
 
 // Load topics and initialize game
